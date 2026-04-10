1944be319d2065af3fc655b78c6ff0223bb63670
Tharushi Shehara <tharuusamaraweera2003@gmail.com>
2026-04-10 15:15:17 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import Movies from './pages/Movies'
+import TvShows from './pages/TvShows'
+import MyList from './pages/MyList'
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Home />} />
+        <Route path="/tv-shows" element={<Home />} />
+        <Route path="/my-list" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
