bd6741433ea911e4783811178f28886d9e7b1139
Tharushi Shehara <tharuusamaraweera2003@gmail.com>
2026-04-12 21:04:06 +0530

    home page developing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,16 @@ import'./css/Navbar.css'
 export default function Navbar() {
   return (
     <nav className='nav bg-gray-900 text-white p-2 flex justify-between items-center shadow-[0_4px_20px_rgba(60,120,246,0.8)]'>
-        <h1 className='text-2xl p-5 font-bold'>Cine<span className='text-yellow-600'>Vault</span></h1>
-        <ul className='flex justify-right p-2 ml-50'>
-           <li><button className='navbtn hover:text-yellow-400 transform hover:scale-105 cursor-pointer'><a href="#">Home</a></button></li>
-           <li><button className='navbtn hover:text-yellow-400 transform hover:scale-105 cursor-pointer'><a href="#">Movies</a></button></li>
-           <li><button className='navbtn hover:text-yellow-400 transform hover:scale-105 cursor-pointer'><a href="#">TV Shows</a></button></li>
-           <li><button className='navbtn hover:text-yellow-400 transform hover:scale-105 cursor-pointer'><a href="#">My List</a></button></li>
+        <h1 className='p-5 text-2xl font-bold'>Cine<span className='text-yellow-600'>Vault</span></h1>
+        <ul className='flex p-2 justify-right ml-50'>
+           <li><button className='transform cursor-pointer duration-400 navbtn hover:text-yellow-400 hover:scale-105'><a href="#">Home</a></button></li>
+           <li><button className='transform cursor-pointer duration-400 navbtn hover:text-yellow-400 hover:scale-105'><a href="#">Movies</a></button></li>
+           <li><button className='transform cursor-pointer duration-400 navbtn hover:text-yellow-400 hover:scale-105'><a href="#">TV Shows</a></button></li>
+           <li><button className='transform cursor-pointer duration-400 navbtn hover:text-yellow-400 hover:scale-105'><a href="#">My List</a></button></li>
         </ul>
-        <div className='flex items-center box rounded-full overflow-hidden w-90 shadow-[0_8px_20px_rgb(0,0,0.9)] transform hover:scale-101'>
-            <input type="text" placeholder='Type here...' className='bg-transparent focus:outline-none text-white p-4 hover:bg-gray-800' />
-        <button className='btn bg-blue-800 p-2 w-full hover:bg-blue-700 cursor-pointer'>Search</button>
+        <div className='flex items-center box rounded-full overflow-hidden w-90 shadow-[0_8px_20px_rgb(0,0,0.9)] transform duration-400 hover:scale-101'>
+            <input type="text" placeholder='Type here...' className='p-4 text-white bg-transparent focus:outline-none hover:bg-gray-800' />
+        <button className='w-full p-2 bg-blue-800 cursor-pointer btn hover:bg-blue-700'>Search</button>
         </div>
     </nav>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,76 @@
-import React from 'react'
-import './Home.css'
-import img1 from './images/img1.jpg';
-import img2 from './images/img2.jpg';
-import img3 from './images/img3.jpg';
-import img4 from './images/img4.jpg';
-import img5 from './images/img5.jpg';
+import React from "react";
+import "./Home.css";
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
+import img4 from "./images/img4.jpg";
 
 export default function Home() {
   return (
     <>
+      <div className="w-full p-0 mx-0">
+        <div className="flex justify-between gap-10 p-2">
+          <div className="relative justify-start w-full overflow-hidden bg-gray-800 rounded h-175 flex-box">
+            <img
+              src={img1}
+              alt="movie-img"
+              className="absolute w-full h-full "
+            />
+            <img
+              src={img2}
+              alt="movie-img"
+              className="absolute w-full h-full"
+            />
+            <img
+              src={img3}
+              alt="movie-img"
+              className="absolute w-full h-full"
+            />
+            <img
+              src={img4}
+              alt="movie-img"
+              className="absolute w-full h-full"
+            />
+          </div>
 
-    <div className='container'>
-        <h1 className='mt-20 text-6xl font-bold text-center text-white ml-50'>Welcome to <span className='text-transparent bg-linear-to-r from-blue-400 to-purple-400 bg-clip-text'>CineVault...</span></h1>
-    </div>
+          <div className="justify-end w-full mx-10 bg-transparent h-120">
+            <h1 className="mt-40 mb-3 text-5xl font-bold text-center text-gray-400">
+              Welcome to{" "}
+              <span className="text-transparent bg-linear-to-r from-blue-400 to-purple-400 bg-clip-text">
+                CineVault...
+              </span>
+            </h1>
 
-    <div className='container flex justify-between gap-10 mt-20'>
-        <div className='relative justify-start w-3/5 p-5 ml-10 overflow-hidden bg-gray-800 rounded-lg flex-box h-120'>
-           <img src={img1} alt="movie-img" className='absolute w-full h-full animate-float1' />
-           <img src={img2} alt="movie-img" className='absolute w-full h-full animate-float2' />
-           <img src={img3} alt="movie-img" className='absolute w-full h-full animate-float3' />
-           <img src={img4} alt="movie-img" className='absolute w-full h-full animate-float4' />
+            <div className="w-full h-8 bg-gray-600">
+              
+            </div>
+
+            <p className="mb-10 text-2xl font-bold text-center text-yellow-500">
+              Your world of movies and series starts here.
+            </p>
+            <p className="mt-5 text-lg text-justify text-white">
+              CineVault is a modern streaming platform where you can explore the
+              latest movies and popular TV shows in one place. Discover trending
+              titles, browse by genre, and find something to watch within
+              seconds. Whether you enjoy action, drama, comedy, or sci-fi,
+              CineVault brings together a wide collection for every mood. Save
+              your favorites, track what you love, and enjoy a smooth, simple
+              viewing experience designed for everyone.
+            </p>
+            <div className="flex justify-center gap-10 mt-20">
+              <button className="px-6 py-8 text-3xl text-transparent transition duration-500 border-2 border-blue-400 rounded-lg cursor-pointer bg-linear-to-r from-blue-500 to-purple-500 bg-clip-text hover:bg-blue-600 hover:text-blue-600 transmition hover:scale-105 hover:border-blue-600 hover:shadow-[0_8px_20px_rgb(10,20,50.9)">
+                Explore Now
+              </button>
+              <button className="px-6 py-8 text-3xl text-transparent transition duration-500 border-2 border-blue-400 rounded-lg cursor-pointer bg-linear-to-r from-blue-500 to-purple-500 bg-clip-text hover:bg-blue-600 hover:text-blue-600 transmition hover:scale-105 hover:border-blue-600 hover:shadow-[0_8px_20px_rgb(10,20,50.9)">
+                Explore Now
+              </button>
+              <button className="px-6 py-8 text-3xl text-transparent transition duration-500 border-2 border-blue-400 rounded-lg cursor-pointer bg-linear-to-r from-blue-500 to-purple-500 bg-clip-text hover:bg-blue-600 hover:text-blue-600 transmition hover:scale-105 hover:border-blue-600">
+                Contact Us
+              </button>
+            </div>
+          </div>
         </div>
-        
-        <div className='justify-end w-2/5 p-5 bg-transparent h-120'>
-             <p className='justify-start text-4xl text-yellow-300'>Watch new releses Movies and TV series in one place...</p>
-        </div>
-    </div>
-
+      </div>
     </>
-  )
-
+  );
 }
